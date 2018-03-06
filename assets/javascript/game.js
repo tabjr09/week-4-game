af342be12dbf1d904865b23eae56c98d70a728d2
Thomas Brown <TJ@Thomass-MacBook-Pro.local>
2018-03-05 20:03:36 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,6 +36,9 @@ function Initialize (){
     // Each imageCrystal will be given a src link to the crystal image
         imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
+        //imageCrystal.attr("src", "week-4-game/week-4-game/assets/images/diamondgif.gif");
+
+
         imageCrystal.attr("data-crystalvalue", crystalnumber);
 
         crystals.append(imageCrystal);
